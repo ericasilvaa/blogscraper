fb00606efc45d99e4c48eb91ee08027554070996
Odorico Veloso <odoricoveloso@gmail.com>
2024-12-30 19:43:06 -0300

    Update techTudoScraper.py

--- techTudoScraper.py
+++ techTudoScraper.py
@@ -117,12 +117,11 @@ def listar_noticias_categoria(categoria, categoria_url, qtd_noticias, chrome_opt
     lista_noticias = soup.select('div[data-type="materia"]')
     logger.info(f'Quantidade de notícias encontradas: {len(lista_noticias)}')
 
-    logger.info('Capturando dados das notícias...')
-    for i, noticia in enumerate(lista_noticias[:qtd_noticias], start=1):
+    logger.info('Coletando links das notícias...')
+    for noticia in lista_noticias[:qtd_noticias]:
         try:
             id_noticia = noticia.select_one('div[class^="feed-post"]').get('id')
             titulo = noticia.select_one('div[class*="title"] a').get_text(strip=True)
-            logger.info(f'Capturando notícia {i}/{len(lista_noticias[:qtd_noticias])}: {titulo}')
             link = noticia.select_one('div[class*="title"] a').get('href')
             resumo = noticia.select_one('p[class*="resumo"]').get_text(strip=True)
             noticias[id_noticia] = {
@@ -131,11 +130,33 @@ def listar_noticias_categoria(categoria, categoria_url, qtd_noticias, chrome_opt
                 'link': link
             }
         except Exception as e:
-            logger.error(f'Erro ao capturar notícia: {noticia}, erro: {e}')
+            logger.error(f'Erro ao coletar link da notícia: {noticia}, erro: {e}')
     driver.quit()
     return noticias
 
-
+# Função para acessar cada notícia e coletar os dados
+def salvar_dados_noticia(url_noticia):
+    try:
+        response = requests.get(url_noticia)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            titulo = soup.select_one('h1[itemprop="headline"]').get_text(strip=True)
+            data_publicacao = soup.select_one('time[itemprop="datePublished"]').get('datetime')
+            autor = soup.select_one('address[itemprop="author"] meta[itemprop="name"]').get('content')
+            descricao = soup.select_one('h2[itemprop="alternativeHeadline"]').get_text(strip=True)
+            lista_conteudo = soup.select('div[class*="content-text"]')
+            conteudo = ''.join([p.get_text(strip=True) for p in lista_conteudo])
+            return {
+                'titulo': titulo,
+                'data_publicacao': data_publicacao,
+                'autor': autor,
+                'descricao': descricao,
+                'conteudo': conteudo
+            }
+    except Exception as e:
+        logger.error(f'Erro ao capturar dados da notícia: {url_noticia}, erro: {e}')
+    
+    
 
 # Execução do script
 if __name__ == '__main__':
@@ -153,7 +174,14 @@ if __name__ == '__main__':
             os.system('cls' if os.name == 'nt' else 'clear')
             logger.info(f'Categoria escolhida: {categoria_escolhida}')
             qtd_noticias = int(input('Quantidade de notícias a serem capturadas: '))
-            noticias = listar_noticias_categoria(categoria_escolhida, categorias[categoria_escolhida], qtd_noticias, chrome_options)
+            links_noticias = listar_noticias_categoria(categoria_escolhida, categorias[categoria_escolhida], qtd_noticias, chrome_options)
+            noticias = {}
+            for id_noticia, dados_noticia in links_noticias.items():
+                logger.info(f'Acessando notícia: {dados_noticia["titulo"]}')
+                dados_completos = salvar_dados_noticia(dados_noticia['link'])
+                if dados_completos:
+                    noticias[id_noticia] = {**dados_noticia, **dados_completos}
+                    time.sleep(random.randint(3, 10))
             salvar_json(noticias, f'noticias_{categoria_escolhida}.json')
         else:
             logger.error("Categoria escolhida não é válida.")
